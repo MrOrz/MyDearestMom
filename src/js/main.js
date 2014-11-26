2ee3b93423b5eea17b7f146060827fd98fff85fc
MrOrz <johnsonliang7@gmail.com>
2014-11-27 01:27:18 +0800

    Add loading screen

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,10 @@ var skrollr = require('../../vendor/bower_components/skrollr/dist/skrollr.min');
 require('../styl/main.styl'); // Just trigger CSS compilation & text extraction...
 
 skrollr.init();
+
+window.addEventListener('load', function(){
+  document.body.classList.add('is-loaded');
+  setTimeout(function(){
+    document.querySelector('.loading').remove();
+  }, 500);
+});
