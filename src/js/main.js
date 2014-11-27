cad60e83d4f48434f17c2652779ce433a3120007
MrOrz <johnsonliang7@gmail.com>
2014-11-27 16:34:37 +0800

    Animatable age

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,33 @@
 var skrollr = require('../../vendor/bower_components/skrollr/dist/skrollr.min');
 require('../styl/main.styl'); // Just trigger CSS compilation & text extraction...
 
-skrollr.init();
+var shouldAnimateBirthTime = false, BIRTH_TIME_UPPER_LIMIT=1000,
+    birthTimestamp = +(new Date(1954, 10, 27)),
+    evacuateTimestamp = +(new Date(1955, 1, 8)),
+    timeDelta = evacuateTimestamp - birthTimestamp;
+
+var birthElem = document.getElementById('birth'),
+    birthElemDate = birthElem.querySelector('.date'),
+    birthElemAge = birthElem.querySelector('.age');
+
+skrollr.init({
+  keyframe: function(elem, name, direction){
+    if(elem === birthElem){
+      shouldAnimateBirthTime = (name === 'data0' && direction === 'down') ||
+                               (name === 'data'+BIRTH_TIME_UPPER_LIMIT && direction === 'up')
+      if(!shouldAnimateBirthTime) {
+        if(name === 'data0') updateBirthTime(0);
+        if(name === 'data'+BIRTH_TIME_UPPER_LIMIT) updateBirthTime(BIRTH_TIME_UPPER_LIMIT);
+      }
+
+    }
+  },
+  render: function(data){
+    if(shouldAnimateBirthTime){
+      updateBirthTime(data.curTop);
+    }
+  }
+});
 
 window.addEventListener('load', function(){
   document.body.classList.add('is-loaded');
@@ -9,3 +35,15 @@ window.addEventListener('load', function(){
     document.querySelector('.loading').remove();
   }, 500);
 });
+
+function updateBirthTime(curTop) {
+  var date = new Date(curTop/BIRTH_TIME_UPPER_LIMIT * timeDelta + birthTimestamp),
+      monthCount = (date.getMonth() - 10 + 12)%12;
+  birthElemDate.innerText = date.getFullYear() + '.' + (date.getMonth()+1) + '.' + date.getDate();
+  if(monthCount > 0){
+    birthElemAge.innerText = "0 歲 " + monthCount + " 個月";
+  }else{
+    birthElemAge.innerText = "0 歲"
+  }
+  // console.log(birthTimestamp, newTimestamp, evacuateTimestamp);
+}
